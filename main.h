24bc0ea2d5079bd8d2e2593d2b18a71bb6eb5cc5
practice1 <shson1217@kw.ac.kr>
2023-12-02 16:33:35 +0900

    [update]: 12/2

---
+++ main.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 16:27:52 by marvin            #+#    #+#             */
+/*   Updated: 2023/12/02 16:27:52 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+# include "stdio.h"
+# include "structures.h"
+# include "./util/utils.h"
+# include "./excute/scene.h"
+# include "./excute/trace.h"
+# include "./libft/libft.h"
+
+int    write_color(t_color3 pixel_color);
+
+#endif
